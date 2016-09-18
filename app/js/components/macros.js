4767e37994b23a1148bbd0e332c8597d64cf3338
Jonathan Vasek <jonathan@voshtech.com>
2016-09-18 18:40:09 -0500

    The total amount of wait time per macro is now displayed (in milliseconds).

--- app/js/components/macros.js
+++ app/js/components/macros.js
@@ -31,43 +31,50 @@ angular.module('ffxivCraftOptWeb.components')
           var finishSoundEffect = '<se.' + $scope.options.finishSoundEffect + '>';
 
           var lines = [];
+          var waitTimes = [];
 
           for (var i = 0; i < $scope.sequence.length; i++) {
             var action = $scope.sequence[i];
+            var waitTime = 0;
             var info = _actionsByName[action];
             if (info) {
               var actionName = $translate.instant(info.name);
               var line = '/ac "' + actionName + '" <me> ';
               if (buffs[action]) {
                 line += buffWaitString;
+                waitTime = $scope.options.buffWaitTime;
               }
               else {
                 line += waitString;
+                waitTime = $scope.options.waitTime;
               }
               line += '\n';
               lines.push(line);
+              waitTimes.push(waitTime);
             }
             else {
               lines.push('/echo Error: Unknown action ' + action);
+              waitTimes.push(waitTime);
             }
           }
 
           var macroList = [];
-
-          var macroString = '';
+          
+          var macroData = {macroText: '', macroTotalWait: 0};
           for (var j = 0; j < lines.length; j++) {
-            macroString += lines[j];
+            macroData.macroText += lines[j];
+            macroData.macroTotalWait += waitTimes[j];
             var step = j + 1;
             if (step % maxLines === 0) {
-              macroString += '/echo Macro #' + step / maxLines + ' complete ' + stepSoundEffect + '\n';
-              macroList.push(macroString);
-              macroString = '';
+              macroData.macroText += '/echo Macro #' + step / maxLines + ' complete ' + stepSoundEffect + '\n';
+              macroList.push(macroData);
+              macroData = {macroText: '', macroTotalWait: 0};
             }
           }
 
-          if (macroString !== '') {
-            macroString += '/echo Macro #' + Math.ceil(lines.length / maxLines) + ' complete ' + finishSoundEffect + '\n';
-            macroList.push(macroString)
+          if (macroData.macroText !== '') {
+            macroData.macroText += '/echo Macro #' + Math.ceil(lines.length / maxLines) + ' complete ' + finishSoundEffect + '\n';
+            macroList.push(macroData);
           }
 
           $scope.macroList = macroList;
